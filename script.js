7dd464bb34cf9878b6b9d40709e118ba8f5a2c76
rangeeeeee <rakyung@dankoon.ac.kr>
2024-04-28 20:08:47 +0900

    에러 수정

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 document.addEventListener("DOMContentLoaded", function() {
- 
     const sections = document.querySelectorAll("main section");
     sections.forEach(function(section) {
         section.style.display = "none";
     });
 
-    const firstMenuItem = document.querySelector("nav ul li a");
-    const initialSectionId = firstMenuItem.getAttribute("data-target");
-    document.getElementById(initialSectionId).style.display = "block";
+    const defaultSection = document.getElementById("base");
+    if (defaultSection) {
+        defaultSection.style.display = "block";
+    }
 
-
+    // 모든 메뉴 항목을 선택합니다.
     const menuItems = document.querySelectorAll("nav ul li a");
-
-  
     menuItems.forEach(function(menuItem) {
         menuItem.addEventListener("click", function(event) {
-            
             event.preventDefault();
-
-        
             sections.forEach(function(section) {
                 section.style.display = "none";
             });
 
-         
             const targetSectionId = menuItem.getAttribute("data-target");
             const targetSection = document.getElementById(targetSectionId);
             if (targetSection) {
                 targetSection.style.display = "block";
             }
         });
+    });
+
+ 
+    const titleLink = document.querySelector("h1 a");
+
+   
+    titleLink.addEventListener("click", function(event) {
+        
     });
 });
